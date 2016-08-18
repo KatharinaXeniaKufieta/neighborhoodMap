22249cb037a2ee6b58d9e320c6cf70ee67a89c91
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2016-08-18 19:27:32 -0400

    feat: Initial commit, add map & title

---
+++ README.md
@@ -0,0 +1,2 @@
+# Neighborhood Map
+The neighborhood map displays my and KJs favorite locations in NYC. 
---
+++ js/googleMaps.js
@@ -0,0 +1,11 @@
+// Global variables for the map
+var zoom = 14;
+var map;
+
+// Callback function that is called when google maps is created
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7413549, lng: -73.9980244},
+    zoom: zoom
+  });
+}
