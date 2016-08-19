d22cddd7ad49ff134f250183e6e6b97989568187
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2016-08-19 18:02:29 -0400

    feat: Add Knockout and list of locations

---
+++ js/model.js
@@ -0,0 +1,50 @@
+// These are locations shown to the user. Normally
+// we'd have these in a database instead
+var locations = [
+    {
+      title: 'Variety',
+      location: {lat: 40.715407, lng: -73.944341},
+      description: 'My fav coffee. Not sure I can live without it any more!',
+      iconImage: 'img/icons/coffee.png',
+      descriptionImage: 'http://i.giphy.com/YU0HoCQidyGEE.gif'}, {
+      title: 'Momo Sushi Shack',
+      location: {lat: 40.70512, lng: -73.933463},
+      description: 'Best sushi ever. You will never look at sushi the same way again.',
+      iconImage: 'img/icons/noodles.png',
+      descriptionImage: 'http://i.giphy.com/X2QBmjCQAHtle.gif'}, {
+      title: 'David Barton Gym',
+      location: {lat: 40.740404, lng: -73.993308},
+      description: 'A gym within a church! Working out has just gotten more spiritual and beautiful.',
+      iconImage: 'img/icons/weightlifting.png',
+      descriptionImage: 'http://i.giphy.com/hlh2xvhZOfzji.gif'}, {
+      title: 'Cafe Mogador Williamsburg',
+      location: {lat: 40.719726, lng: -73.959983},
+      description: 'My fav maroccan restaurant. Try the lamb tagine!',
+      iconImage: 'img/icons/noodles.png',
+      descriptionImage: 'http://i.giphy.com/l0O9yqyFbuxZoBifu.gif'}, {
+      title: 'Smorgasburg',
+      location: {lat: 40.72102, lng: -73.962178},
+      description: 'Amazing food in a flea market style flair.',
+      iconImage: 'img/icons/noodles.png',
+      descriptionImage: 'http://i.giphy.com/3F3QVLy3w6OfC.gif'}, {
+      title: 'BARC Shelter',
+      location: {lat: 40.716199, lng: -73.963794},
+      description: 'You can go on walks with dogs here, or hang out with cats.',
+      iconImage: 'img/icons/tiger.png',
+      descriptionImage: 'http://i.giphy.com/eij3Aplt9hquI.gif'}, {
+      title: 'DUMBO Boulders',
+      location: {lat: 40.704272, lng: -73.989235},
+      description: 'Outdoor bouldering.',
+      iconImage: 'img/icons/climbing.png',
+      descriptionImage: 'http://i.giphy.com/NotDYMTH9HUiI.gif'}, {
+      title: 'The Cliffs at LIC',
+      location: {lat: 40.748649, lng: -73.948733},
+      description: 'Awesome climbing gym.',
+      iconImage: 'img/icons/climbing.png',
+      descriptionImage: 'http://i.giphy.com/NotDYMTH9HUiI.gif'}, {
+      title: 'Champs',
+      location: {lat: 40.70844, lng: -73.9409},
+      description: 'My fav vegan diner.',
+      iconImage: 'img/icons/noodles.png',
+      descriptionImage: 'http://i.giphy.com/jKaFXbKyZFja0.gif'}
+  ]
---
+++ js/view.js
@@ -0,0 +1,19 @@
+var Location = function(data) {
+  this.title = ko.observable(data.title);
+  this.location = ko.observable(data.location);
+  this.description = ko.observable(data.description);
+  this.iconImage = ko.observable(data.iconImage);
+  this.descriptionImage = ko.observable(data.descriptionImage);
+  this.liked = false;
+};
+
+var ViewModel = function() {
+  var self = this;
+  this.locations = ko.observableArray([]);
+
+  locations.forEach(function(location) {
+    self.locations.push(new Location(location));
+  });
+}
+
+ko.applyBindings(new ViewModel());
