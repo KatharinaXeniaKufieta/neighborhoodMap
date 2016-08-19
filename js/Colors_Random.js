8ce0668f7980c42c0da40848881ea50d562df09f
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2016-08-19 16:15:40 -0400

    feat: Change colors of map and frame

---
+++ js/Colors_Random.js
@@ -0,0 +1,84 @@
+var styles = [
+  {
+    "featureType": "water",
+    "stylers": [
+      { "color": "#8dcee0" }
+      // { "lightness": "35" }
+    ]
+  },{
+    "featureType": "landscape",
+    "stylers": [
+      { "color": "#e3e7d6" }
+    ]
+  },{
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      { "visibility": "simplified" },
+      { "hue": "#ddff00" },
+      { "color": "#FFFFFF" }
+    ]
+  },{
+    "featureType": "road.highway",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "poi.business",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "poi",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "poi.attraction",
+    "stylers": [
+      { "visibility": "on" }
+    ]
+  },{
+    "featureType": "poi.park",
+    "stylers": [
+      { "visibility": "on"}
+    ]
+  },{
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#a3d99f" }
+      // { "lightness": 39 }
+      // { "hue": "#08ff00" }
+    ]
+  },{
+    "featureType": "poi.sports_complex",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#c3e0bb" }
+    ]
+  },{
+  },{
+    "featureType": "transit"  },{
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "transit.station",
+    "stylers": [
+      { "hue": "#073753" }
+    ]
+  },{
+    "featureType": "transit.line",
+    "stylers": [
+      { "hue": "#f44b34" },
+      { "weight": 3 },
+      { "visibility": "on" }
+    ]
+  }
+];
+// colour palette:
+// http://www.colourlovers.com/palette/1453973/Random
