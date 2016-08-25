07edc166fbf30eaa70ce5b30130691e706c983a3
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2016-08-25 17:04:01 -0400

    feat: Fix highlighting of links & markers

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -1,15 +1,15 @@
 // Global variables for the map
 var zoom = 13;
 var map, bounds;
-var largeInfoWindow, miniInfoWindow, cornerInfoWindow;
+var infoWindow,
+    miniInfoWindow,
+    cornerInfoWindow;
 
 // When google maps has loaded, apply bindings and load map
 var initMap = function() {
   showMap();
-
   ko.applyBindings(new ViewModel());
 }
-
 
 // Callback function that is called when google maps is created
 var showMap = function() {
@@ -23,93 +23,8 @@ var showMap = function() {
   // Boundaries that will be adjusted depending on the markers that are shown
   bounds = new google.maps.LatLngBounds();
   // Infowindows that are shown when a list item or marker is clicked
-  largeInfoWindow = new google.maps.InfoWindow();
+  infoWindow = new google.maps.InfoWindow();
   miniInfoWindow = new google.maps.InfoWindow();
   cornerInfoWindow = document.getElementById('corner-infowindow');
   cornerInfoWindow.className += ' gm-style-iw';
 }
-
-var showInfoWindow = function(marker, infowindow, cornerInfoWindow) {
-  // Respond to CSS3 media query state changes
-  // If min-width is >= 700px, display large info
-  // window in upper left corner.
-  var mq = window.matchMedia( "(min-width: 700px)" );
-  // Open the corner info window,
-  // keep the normal infowindow closed
-  if (mq.matches == true) {
-    // Check to make sure the infowindow is not already opened
-    // on this marker.
-    infowindow.marker = null;
-    infowindow.close();
-    if (cornerInfoWindow.marker != marker) {
-      cornerInfoWindow.marker = marker;
-      cornerInfoWindow.innerHTML = '';
-      cornerInfoWindow.style.visibility = 'visible';
-      // Make sure the marker property is cleared if the infowindow is closed.
-      var streetViewService = new google.maps.StreetViewService();
-      var radius = 50;
-      // In case the status is OK, which means the pano was found, compute
-      // the position of the streetview image, then calculate the heading,
-      // then get a panorama from that and set the options
-      function getStreetView(data, status) {
-        if (status == google.maps.StreetViewStatus.OK) {
-          var nearStreetViewLocation = data.location.latLng;
-          var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
-          cornerInfoWindow.innerHTML = '<div id="close-thick"></div><div id="pano"></div><img class="infowindow-image" src="' + marker.gif + '"><div class="infowindow-text">' + marker.description + '</div>';
-          var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 30
-            }
-          };
-          var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), panoramaOptions);
-        } else {
-          cornerInfoWindow.innerHTML = '<div id="close-thick"></div><img class="infowindow-image" src="' + marker.gif + '"><div class="infowindow-text">' + marker.description + '</div>';
-        }
-        var closebutton = document.getElementById('close-thick');
-        closebutton.addEventListener('click', function() {
-          cornerInfoWindow.style.visibility = 'hidden';
-        }, false);
-      }
-      // Use streetview service to get the closest streetvew image within
-      // 50 meters of the markers position
-      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-    }
-  } else {
-    // Open the normal info window and close the corner info window closed
-    cornerInfoWindow.style.visibility = 'hidden';
-    // Check to make sure the infowindow is not already opened
-    // on this marker.
-    if (infowindow.marker != marker) {
-      infowindow.marker = marker;
-      infowindow.setContent('');
-      infowindow.setContent('<div "class="infowindow-text">' + marker.title + '</div><div "class="infowindow-text">' + marker.description + '</div>');
-      // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick', function() {
-        infowindow.marker = null;
-      });
-      // Open the infowindow on the correct marker
-      infowindow.open(map, marker);
-    }
-  }
-};
-
-var showMinimizedInfoWindow = function(marker, miniInfoWindow, largeInfoWindow, cornerInfoWindow) {
-  // Check to make sure the miniInfoWindow is not already opened
-  // on this marker.
-  if (largeInfoWindow.marker != marker && cornerInfoWindow.marker != marker) {
-    miniInfoWindow.marker = marker;
-    miniInfoWindow.setContent('');
-    miniInfoWindow.open(map, marker);
-    // Make sure the marker property is cleared if the miniInfoWindow is closed.
-    miniInfoWindow.setContent('<div id="min-infowindow "class="infowindow-text">' + marker.title + '</div>');
-    miniInfoWindow.open(map, marker);
-  }
-  // Open the infowindow on the correct marker
-};
-
-var hideMinimizedInfoWindow = function(marker, infowindow) {
-  infowindow.close();
-};
--- js/view.js
+++ js/view.js
@@ -1,4 +1,5 @@
 var Location = function(data) {
+  var self = this;
   // Knockout observables
   this.title = ko.observable(data.title);
   this.position = ko.observable(data.position);
@@ -7,7 +8,7 @@ var Location = function(data) {
   this.gif = ko.observable(data.gif);
   this.id = ko.observable(data.id);
   this.liked = ko.observable(data.liked);
-  this.highlighted = false;
+  this.hover = ko.observable(false);
 
   // Markers are not supposed to be observables
   // Create a new marker for each location
@@ -22,32 +23,57 @@ var Location = function(data) {
     animation: google.maps.Animation.DROP
   });
 
-  this.marker.addListener('mouseover', function() {
-    showMinimizedInfoWindow(this, miniInfoWindow, largeInfoWindow, cornerInfoWindow);
-    this.highlighted = true;
-  });
-  this.marker.addListener('mouseout', function() {
-    hideMinimizedInfoWindow(this, miniInfoWindow);
-    this.highlighted = false;
-  });
-  this.marker.addListener('click', function() {
-    showInfoWindow(this, largeInfoWindow, cornerInfoWindow);
-    hideMinimizedInfoWindow(this, miniInfoWindow);
-    this.highlighted = true;
+  this.getHighlight = ko.pureComputed(function() {
+    return self.hover() ? 'hover' : 'noHighlight';
   });
 };
 
-/***********
- * ViewModel
- ***********/
-
+/*************
+ * ViewModel *
+ *************/
 var ViewModel = function() {
   var self = this;
   // put the locations into an observable array
   this.locations = ko.observableArray([]);
+  this.selectedLocationId = ko.observable(null);
 
-  locations.forEach(function(location) {
-    self.locations.push(new Location(location));
+  locations.forEach(function(loc) {
+    var location = new Location(loc);
+
+    location.marker.addListener('mouseover', (function(thisLocation) {
+      return function() {
+        self.showMinimizedInfoWindow(thisLocation.marker);
+        thisLocation.hover(true);
+      }
+    })(location));
+
+    location.marker.addListener('mouseout', (function(thisLocation) {
+      return function() {
+        self.hideMinimizedInfoWindow(thisLocation.marker);
+        thisLocation.hover(false);
+      };
+    })(location));
+
+    location.marker.addListener('click', (function(thisLocation) {
+      return function() {
+        var newLocationId = thisLocation.id();
+        self.selectedLocationId(thisLocation.id());
+        self.showInfoWindow(thisLocation);
+      };
+    })(location));
+
+
+    location.getSelected = ko.pureComputed(function() {
+      if (location.hover()) {
+        return 'hover';
+      } else if (self.selectedLocationId() === location.id()) {
+        return 'selected';
+      } else {
+        return 'noHighlight';
+      }
+    }, location);
+
+    self.locations.push(location);
   });
 
   // Set the map for each marker, extend boundaries to encompass
@@ -61,8 +87,123 @@ var ViewModel = function() {
   map.fitBounds(bounds);
 
   this.chooseLocation = function(location) {
-    showInfoWindow(location.marker, largeInfoWindow, cornerInfoWindow);
-    location.highlighted = true;
+    self.showInfoWindow(location);
+  };
+
+  this.mouseOver = function(location) {
+    self.showMinimizedInfoWindow(location.marker);
+    location.hover(true);
+  };
+
+  this.mouseOut = function(location) {
+    self.hideMinimizedInfoWindow(location.marker);
+    location.hover(false);
   };
 };
 
+
+/******************************
+ * ViewModel - showInfoWindow *
+ ******************************/
+ViewModel.prototype.showInfoWindow = function(location) {
+  var self = this;
+  // Hide the minimixed infowindow
+  this.hideMinimizedInfoWindow(location.marker);
+  // set the selectedLocationId to the id of the currently chosen
+  // location
+  this.selectedLocationId(location.id());
+  var marker = location.marker;
+  // Respond to CSS3 media query state changes
+  // If min-width is >= 700px, display large info
+  // window in upper left corner.
+  var mq = window.matchMedia( "(min-width: 700px)" );
+  // Open the corner info window,
+  // keep the normal infoWindow closed
+  if (mq.matches == true) {
+    // Check to make sure the infoWindow is not already opened
+    // on this marker.
+    infoWindow.marker = null;
+    infoWindow.close();
+    if (cornerInfoWindow.marker != marker) {
+      cornerInfoWindow.marker = marker;
+      cornerInfoWindow.innerHTML = '';
+      cornerInfoWindow.style.visibility = 'visible';
+      // Make sure the marker property is cleared if the infoWindow is closed.
+      var streetViewService = new google.maps.StreetViewService();
+      var radius = 50;
+      // In case the status is OK, which means the pano was found, compute
+      // the position of the streetview image, then calculate the heading,
+      // then get a panorama from that and set the options
+      function getStreetView(data, status) {
+        if (status == google.maps.StreetViewStatus.OK) {
+          var nearStreetViewLocation = data.location.latLng;
+          var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
+          cornerInfoWindow.innerHTML = '<div id="close-thick"></div><div id="pano"></div><img class="infowindow-image" src="' + marker.gif + '"><div class="infowindow-text">' + marker.description + '</div>';
+          var panoramaOptions = {
+            position: nearStreetViewLocation,
+            pov: {
+              heading: heading,
+              pitch: 30
+            }
+          };
+          var panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), panoramaOptions);
+        } else {
+          cornerInfoWindow.innerHTML = '<div id="close-thick"></div><img class="infowindow-image" src="' + marker.gif + '"><div class="infowindow-text">' + marker.description + '</div>';
+        }
+        var closebutton = document.getElementById('close-thick');
+        closebutton.addEventListener('click', function() {
+          cornerInfoWindow.style.visibility = 'hidden';
+          cornerInfoWindow.marker = null;
+          self.selectedLocationId(null);
+        }, false);
+      }
+      // Use streetview service to get the closest streetvew image within
+      // 50 meters of the markers position
+      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    }
+  } else {
+    // Open the normal info window and close the corner info window closed
+    cornerInfoWindow.style.visibility = 'hidden';
+    cornerInfoWindow.marker = null;
+    // Check to make sure the infowindow is not already opened
+    // on this marker.
+    if (infoWindow.marker != marker) {
+      infoWindow.marker = marker;
+      infoWindow.setContent('');
+      infoWindow.setContent('<div "class="infowindow-text">' + marker.title + '</div><div "class="infowindow-text">' + marker.description + '</div>');
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infoWindow.addListener('closeclick', function() {
+        infoWindow.marker = null;
+        self.selectedLocationId(null);
+      });
+      // Open the infowindow on the correct marker
+      infoWindow.open(map, marker);
+    }
+  }
+};
+
+/***************************************
+ * ViewModel - showMinimizedInfoWindow *
+ ***************************************/
+ViewModel.prototype.showMinimizedInfoWindow = function(marker) {
+  // Check to make sure the miniInfoWindow is not already opened
+  // on this marker.
+  if (infoWindow.marker != marker && cornerInfoWindow.marker != marker) {
+    miniInfoWindow.marker = marker;
+    miniInfoWindow.setContent('');
+    miniInfoWindow.open(map, marker);
+    // Make sure the marker property is cleared if the miniInfoWindow is closed.
+    miniInfoWindow.setContent('<div id="min-infowindow "class="infowindow-text">' + marker.title + '</div>');
+    miniInfoWindow.open(map, marker);
+  }
+  // Open the infowindow on the correct marker
+};
+
+/***************************************
+ * ViewModel - hideMinimizedInfoWindow *
+ ***************************************/
+ViewModel.prototype.hideMinimizedInfoWindow = function(marker) {
+  miniInfoWindow.close();
+  miniInfoWindow.marker = null;
+};
