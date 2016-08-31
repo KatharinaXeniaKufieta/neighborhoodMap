15ec7dc06368dbb227b2b995130fa5dfb2c83eee
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2016-08-31 15:51:19 -0400

    feat: Improve weather report
    * TO DO:
    * Styling, small changes in text
    * Better modularity with knockout

--- js/view.js
+++ js/view.js
@@ -1,5 +1,33 @@
 var innerHTMLtext = '';
 
+var getWeekDay = function(day) {
+  switch (day) {
+  case 1:
+    return 'Monday';
+    break;
+  case 2:
+    return 'Tuesday';
+    break;
+  case 3:
+    return 'Wednesday';
+    break;
+  case 4:
+    return 'Thursday';
+    break;
+  case 5:
+    return 'Friday';
+    break;
+  case 6:
+    return 'Saturday';
+    break;
+  case 0:
+    return 'Sunday';
+    break;
+  default:
+    return 'SOMETHING IS WRONG HERE, toDay: ' + day;
+  }
+}
+
 var Location = function(data) {
   var self = this;
   // Knockout observables
@@ -365,12 +393,15 @@ var foursquareDetails = function(location, text) {
 }
 
 var Weather = function(data) {
-  console.log('from: ' + data.from);
-  console.log('to: ' + data.to);
   var from = new Date(data.from);
   var to = new Date(data.to);
-  console.log('converted from: ' + from);
-  console.log('converted to: ' + to);
+  // Correct and convert dates (4 hours behind)
+  this.timeString = '';
+  this.dayString = '';
+  this.setTimeAndDate(from, to);
+  console.log('timeString: ' + this.timeString);
+  console.log('dayString: ' + this.dayString);
+
   this.from = ko.observable(from);
   this.to = ko.observable(to);
   this.precipitation = ko.observable(data.precipitation.value);
@@ -379,21 +410,98 @@ var Weather = function(data) {
   this.windSpeed = ko.observable(data.windSpeed.name);
 }
 
+Weather.prototype.setTimeAndDate = function(from, to) {
+  // Correct and get the time string
+  var fromHour = from.getHours();
+  var toHour = to.getHours();
+  var newfromHour = (fromHour + 4) % 24;
+  var newtoHour = (toHour + 4) % 24;
+
+  var fromDay = from.getDay();
+  var toDay = to.getDay();
+
+  var fromDate = from.getDate();
+  var toDate = to.getDate();
+  var newfromDate = fromDate + Math.floor((fromHour + 4) / 24);
+  var newtoDate = toDate + Math.floor((toHour + 4) / 24);
+  // Set the corrected hour and day
+  from.setHours(newfromHour);
+  to.setHours(newtoHour);
+
+  from.setDate(newfromDate);
+  to.setDate(newtoDate);
+
+  // Get the string describing the day (Today, Tomorrow, or weekday after)
+  var today = new Date(Date.now());
+  toDay = to.getDay();
+  if (today.getDay() === toDay) {
+    this.dayString += 'Today, '
+  } else if (today.getDay() + 1 === toDay) {
+    this.dayString += 'Tomorrow, '
+  }
+  this.dayString += getWeekDay(toDay);
+
+  if (from.getHours() < 10) {
+    this.timeString += '0' + from.getHours() +  ':00 - ';
+  } else {
+    this.timeString += from.getHours() +  ':00 - ';
+  }
+  if (to.getHours() < 10) {
+    this.timeString += '0' + to.getHours() + ':00';
+  } else {
+    this.timeString += to.getHours() + ':00';
+  }
+}
+
 var WeatherViewModel = function(results) {
   var self = this;
   this.creditLinkText = results.weatherdata.credit.link.text;
   this.creditLinkURL = results.weatherdata.credit.link.url;
-  this.sunrise = results.weatherdata.sun.rise;
-  this.sunset = results.weatherdata.sun.set;
+  this.sunrise = 'Sunrise: ' + results.weatherdata.sun.rise;
+  this.sunset = 'Sunset: ' + results.weatherdata.sun.set;
+  this.weatherForecast = {
+    day01: {
+      name: '',
+      weather: []
+    },
+    day02: {
+      name: '',
+      weather: []
+    },
+    day03: {
+      name: '',
+      weather: []
+    }
+  };
 
-  this.weatherSlots = ko.observableArray([]);
+  // Get the current date strings for day01, day02, day03
+  this.setDateStrings();
+
 
   var result = results.weatherdata.forecast.tabular;
-  for (var i = 0, max = 15; i < max; i++) {
+  for (var i = 0, max = 9; i < max; i++) {
     console.log('-------forecast tabular: -------');
-    var weatherSlot = new Weather(result[i]);
-    self.weatherSlots.push(weatherSlot);
-    // console.log(result[i]);
+    var weather = new Weather(result[i]);
+    if (weather.dayString === this.weatherForecast.day01.name) {
+      this.weatherForecast.day01.weather.push(weather);
+    } else if (weather.dayString === this.weatherForecast.day02.name) {
+      this.weatherForecast.day02.weather.push(weather);
+    } else {
+      this.weatherForecast.day03.weather.push(weather);
+    }
   }
 };
+
+WeatherViewModel.prototype.setDateStrings = function() {
+  var today = new Date(Date.now());
+  todayDay = today.getDay();
+  // push day string for today, day01
+  this.weatherForecast.day01.name = 'Today, ' + getWeekDay(todayDay);
+  // push day string for tomorrow, day02
+  console.log(weatherForecast.day02.name);
+  this.weatherForecast.day02.name = 'Tomorrow, ' + getWeekDay((todayDay + 1)%7);
+  // push day string for the day after tomorrow, day03
+  console.log(weatherForecast.day03.name);
+  this.weatherForecast.day03.name = getWeekDay((todayDay + 2)%7);
+}
 
